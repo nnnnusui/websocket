cbac8849508182f57ace83315455fb13a6316117
nnnnusui <nnnnusui@gmail.com>
2019-04-11 21:07:42 +0900

    :recycle: update: interface [ Event -> IsEvent ]
    I try to add "Is" to the interface representing the types parent.
    型の親(のみ)を表すinterfaceを IsXxx と命名してみることにした。

--- src/main/kotlin/com/github/nnnnusui/event/IsEvent.kt
+++ src/main/kotlin/com/github/nnnnusui/event/IsEvent.kt
@@ -1,3 +1,3 @@
 package com.github.nnnnusui.event
 
-interface Event
+interface IsEvent
--- src/main/kotlin/com/github/nnnnusui/event/Manager.kt
+++ src/main/kotlin/com/github/nnnnusui/event/Manager.kt
@@ -11,11 +11,11 @@ import kotlin.reflect.full.starProjectedType
 
 class Manager: CoroutineScope {
     override val coroutineContext = Dispatchers.Default
-    private val queue    = LinkedBlockingDeque<Event>()
-    private val handlers = mutableMapOf< Any, Array<KFunction<Event>> >()
+    private val queue    = LinkedBlockingDeque<IsEvent>()
+    private val handlers = mutableMapOf< Any, Array<KFunction<IsEvent>> >()
     var alive = true
 
-//    init { Thread({ while(alive) dispatchEvent() }, "Event Dispatcher").start() }
+//    init { Thread({ while(alive) dispatchEvent() }, "IsEvent Dispatcher").start() }
     init { launch { while (alive) dispatchEvent() } }
     private fun dispatchEvent() {
         val event = queue.take()
@@ -26,13 +26,13 @@ class Manager: CoroutineScope {
     }
 
     fun register(handler: Any){ handlers[handler] = getAnnotatedFunctions(handler) }
-    fun fire(event: Event)    { queue.offer(event) }
+    fun fire(isEvent: IsEvent)    { queue.offer(isEvent) }
 
     @Suppress("UNCHECKED_CAST") // :'(
-    private fun getAnnotatedFunctions(instance: Any): Array<KFunction<Event>> // if <out Event>, Be told "Projection is redundant:"
+    private fun getAnnotatedFunctions(instance: Any): Array<KFunction<IsEvent>> // if <out IsEvent>, Be told "Projection is redundant:"
                   = instance::class.members
         /* is function          */ .filter { it is KFunction }
         /* arguments.size == 1  */ .filter { it.parameters.size == 2 }
-        /* fun(arg: <T: Event>) */ .filter { (it.parameters[1].type.classifier as KClass<*>).isSubclassOf(Event::class) }
-                                   .map    { it as KFunction<Event> }.toTypedArray()
+        /* fun(arg: <T: IsEvent>) */ .filter { (it.parameters[1].type.classifier as KClass<*>).isSubclassOf(IsEvent::class) }
+                                   .map    { it as KFunction<IsEvent> }.toTypedArray()
 }
